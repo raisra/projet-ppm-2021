c3b1c6e153646ba5435e1f178fa1cec6cc027d71
ramzi <rais.ramzi@gmail.com>
2021-01-20 22:44:43 +0100

    modification de gitignore pr pusher les fichiers de conf nécessaires à la compile

--- projet-ppm/MessageViewController.swift
+++ projet-ppm/MessageViewController.swift
@@ -32,21 +32,33 @@ class MessageViewController: UIViewController{
         NotificationCenter.default.addObserver(self, selector: #selector(MessageViewController.keyboardWillHide), name: UIWindow.keyboardWillHideNotification, object: nil)
     }
     
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("keyboardwillHide")
+        if(keyBoarAppeared){
+            
+        }
+    }
   
     @objc func keyboardWillShow(notification: NSNotification) {
         print("keyboardwillshow")
         let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
         messageView?.frame.origin.y -= keyboardFrame.cgRectValue.height
         messageView?.setNeedsDisplay()
+        keyBoarAppeared = true
     }
     
+    var keyBoarAppeared: Bool = false
     @objc func keyboardWillHide(notification: NSNotification) {
         print("keyboardwillHide")
         let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
         
         messageView?.frame.origin.y += keyboardFrame.cgRectValue.height
         messageView?.setNeedsDisplay()
+        keyBoarAppeared = false
     }
+    
+    func
     
     @objc func backToGameView()  {
         self.dismiss(animated: true, completion: nil)
